7cd073fb1120275a5e01d2ce5ed44a3ca4064860
Kaan Kuscu <me@kaanksc.com>
2024-06-20 21:26:26 +0300

    remove frontend folder (svelte)

--- frontend/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.b78bccce.js","_app/immutable/chunks/scheduler.643286d3.js","_app/immutable/chunks/index.c2323441.js"];
-export const stylesheets = ["_app/immutable/assets/2.49338e76.css"];
-export const fonts = [];
